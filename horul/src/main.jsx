a427fe2947fd7458afd24cd825b22b5373b43e05
Heckerman2281337 <maskskozevnikov4@gmail.com>
2026-06-06 13:27:44 +0300

    Added faq demo page. Fixed header gap in purchase page

--- horul/src/main.jsx
+++ horul/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import App from './App.jsx'
 
+document.body.style.overflowY = 'scroll' 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
--- horul/src/pages/Faq.jsx
+++ horul/src/pages/Faq.jsx
@@ -1,10 +1,34 @@
-import { Container } from 'react-bootstrap';
+import { Container, Accordion, AccordionItem } from 'react-bootstrap';
+import { FaQuestion } from 'react-icons/fa';
 
 export default function Faq() {
     return (
-        <Container className="py-5">
-            <h1>Часто задаваемые вопросы</h1>
-            <p>Здесь список вопросов</p>
+        <Container className="text-center py-5 mt-5 mb-5" style={{ maxWidth: '700px' }}>
+
+            <h1 className="display-5 fw-bold mb-5">Часто задаваемые вопросы</h1>
+
+            <Accordion defaultActiveKey="1">
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header><FaQuestion size={20} className="text-primary me-3"/> Вопрос 1</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                    <Accordion.Header> <FaQuestion size={20} className="text-primary me-3" /> Вопрос 2</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                    <Accordion.Header> <FaQuestion size={20} className="text-primary me-3" /> Вопрос 3</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet
+                    </Accordion.Body>
+                </Accordion.Item>
+
+            </Accordion>
+
         </Container>
     );
 }
